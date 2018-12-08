9c9b49205d9c858f975fec1f9520812c96bf8beb
Ratanak Phang <phangratanak168@gmail.com>
2018-12-08 11:35:56 +0700

    Add Security

---
+++ src/main/java/com/example/demothymleaffragmentlayout/configurations/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.demothymleaffragmentlayout.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web
+                .ignoring()
+                .antMatchers( "/resources/alte/**");
+    }
+
+}
--- src/main/java/com/example/demothymleaffragmentlayout/controllers/AdminController.java
+++ src/main/java/com/example/demothymleaffragmentlayout/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.demothymleaffragmentlayout.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,5 +15,26 @@ public class AdminController {
     @GetMapping(value = {"/user"})
     public String userPage() {
         return "admin/user";
+    }
+
+
+
+    @GetMapping("/dashboard1-ajax")
+    public String getFragmentContentFromDB1(Model model) {
+
+        model.addAttribute("new_orders", 10_000_000);
+
+        return "admin/dashboard::dashboard1";
+
+    }
+
+    @GetMapping("/dashboard2-ajax")
+    public String getFragmentContentFromDB2() {
+
+
+
+
+        return "admin/dashboard::dashboard2";
+
     }
 }
